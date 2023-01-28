39cd5a96d3ab5858f66f2ff43f7f2b39555f8cf9
monokemonoke <kawabataryuhei@gmail.com>
2023-01-28 22:45:44 +0900

    :recycle: addを処理する関数を分離

--- go-lisp/eval.go
+++ go-lisp/eval.go
@@ -10,13 +10,17 @@ func Eval(ast Ast) any {
 
 	switch astExpression.operand {
 	case "+":
-		sum := 0
-		for _, val := range astExpression.args {
-			num, _ := strconv.Atoi(val)
-			sum += num
-		}
-		return sum
+		return add(astExpression.args)
 	default:
 		return fmt.Sprintf("got unexpected operator %s\n", astExpression.operand)
 	}
+}
+
+func add(args []string) int {
+	sum := 0
+	for _, val := range args {
+		num, _ := strconv.Atoi(val)
+		sum += num
+	}
+	return sum
 }
